b8ec3a2149ada5dfae8307e6154746a796c07578
1109code <1109ssh.code@gmail.com>
2022-08-28 22:26:38 +0900

    great

---
+++ SON/BOJ_1662/BOJ_1662_fail.py
@@ -0,0 +1,29 @@
+# BOJ_1662 문제풀이
+# 2022-08-27
+
+def com():
+    global number
+    global a
+    if '(' not in a:
+        return a
+
+    while a[-1] == ')':
+        a.pop()
+    while a[-1] != '(':
+        stack.append(a.pop())
+    while stack:
+        number += stack.pop()
+    a.pop()
+    number = int(a.pop()) * number
+
+    while a and a[-1] != ')' and a[-1] != '(':
+        number = a.pop() + number
+        if not a:
+            return number
+    return com()
+
+
+a = list(input())
+stack = []
+number = ''
+print(len(com()))
